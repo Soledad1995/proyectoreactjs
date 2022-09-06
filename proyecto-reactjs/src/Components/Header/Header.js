5d3616f4260b973aac344aeedb0c1568e6313b30
Soledad Figueroa <figue.soled@gmail.com>
2022-09-06 19:09:46 -0300

    creando navbar

---
+++ proyecto-reactjs/src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../../assets/logomiosinfondo.png"
+import "./header.css"
+
+const Header = () =>{
+    return(
+        <>
+            <img src="(logo)" alt="logo"></img>
+            <h1> Titulo </h1>
+            <nav>
+                <a href="vinilos.html"> Vinilos</a>
+                <a href="carteleria.html">Carteleria</a>
+                <a href="letras.html">Letras Corporeas</a>
+            </nav>
+            <img src="" alt="Carrito"></img>
+        </>
+    
+    )
+}
+
+export default Header
