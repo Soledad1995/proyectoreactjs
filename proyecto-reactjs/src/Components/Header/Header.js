499d406058e0d0f77c121f30fdbd0df718e10c5a
Soledad Figueroa <figue.soled@gmail.com>
2022-09-08 23:07:57 -0300

    cambios

--- proyecto-reactjs/src/Components/Header/Header.js
+++ proyecto-reactjs/src/Components/Header/Header.js
@@ -2,20 +2,36 @@ import React from "react";
 import logo from "../../assets/logomiosinfondo.png"
 import "./header.css"
 
+
 const Header = () =>{
     return(
-        <>
-            <img src="(logo)" alt="logo"></img>
+        <header style={style.container}>
+            <img style={style.imagen} src={logo} alt="logo"></img>
             <h1> Titulo </h1>
             <nav>
-                <a href="vinilos.html"> Vinilos</a>
-                <a href="carteleria.html">Carteleria</a>
-                <a href="letras.html">Letras Corporeas</a>
+                <a style={style.links} href="vinilos.html"> Vinilos</a>
+                <a style={style.links} href="carteleria.html">Carteleria</a>
+                <a style={style.links} href="letras.html">Letras Corporeas</a>
             </nav>
-            <img src="" alt="Carrito"></img>
-        </>
+            
+        </header>
     
     )
 }
+const style = {
+    container:{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    imagen: {
+        width: '10%',
+    },
+    links: {
+        padding: 10,
+    }
+}
+
+
 
 export default Header
