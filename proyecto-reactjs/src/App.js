06aa65cbdf9575a581ea4e9c4880d0e45081da27
Soledad Figueroa <figue.soled@gmail.com>
2022-09-18 23:53:44 -0300

    agregar boton

--- proyecto-reactjs/src/App.js
+++ proyecto-reactjs/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar"
 import ItemListContainer from "./ItemListContainer";
-
+import ItemCount from "../../ItemCount";
 const App = () => {
   const mensaje = "Hola a todos los que visitan esta pagina"
   return (
--- proyecto-reactjs/src/Components/Container/ItemListContainer/ItemListContainer.js
+++ proyecto-reactjs/src/Components/Container/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,12 @@
 import React from "react";
+import ItemCount from "../../ItemCount";
 
+const onAdd = (quantify) =>{
+    console.log ('compraste ${quantify} unidades');
+}
 const ItemListContainer = ({greeting}) => {
     return (
-        <h1>{greeting}</h1>
+        <><h1>{greeting}</h1><ItemCount initial={1} stock={5} onAdd={onAdd} /></>
     )
 }
 export default ItemListContainer
